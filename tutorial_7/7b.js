b6de7c92f8c2be1e4d5ebdacdeac7fee165e1575
Magdalena Zembrzuska <s20983@pjwstk.edu.pl>
2021-11-24 23:03:39 +0100

    first commit

---
+++ tutorial_7/7b.js
@@ -0,0 +1,1 @@
+// b)	Write a nodejs script that watches directory supplied as a parameter and prints out contents of files that are modified to console. Use https://nodejs.org/api/fs.html to learn about the fs module methods, pay attention to the watch method 😉
