fd95f2e392c2096ccd99cff635fc15ef929ef08c
Magdalena Zembrzuska <s20983@pjwstk.edu.pl>
2021-12-09 23:37:01 +0100

    Update 7a.js

--- tutorial_7/7a.js
+++ tutorial_7/7a.js
@@ -2,7 +2,7 @@
 //1. accept requests for different mathematical operations (add, sub, mul, div) under different URLs
 //2. extract parameter values from the request parameters 
 //3. and returns result in form of HTML response containing the request parameters and result (formatting is up to you). 
-//4. TODO Make sure to handle situations in which the parameters are incorrect (missing or wrong type). 
+//4. Make sure to handle situations in which the parameters are incorrect (missing or wrong type). 
 //You can use https://nodejs.org/api/http.html as reference for the http module.
 
 var http = require('http');
@@ -13,22 +13,31 @@ var server = http.createServer(function (request, response) {
     var num1 = q.number1;
     var num2 = q.number2;
     response.writeHead(200, {'Content-Type': 'text/html'});
-    switch (request.url) {
-        case "/add":
-            response.end(num1 + "+" + num2 + "=" + (num1+num2));
-            break;
-        case "/sub":
-            response.end(num1 + "-" + num2 + "=" + (num1-num2));
-            break;
-        case "/mul":
-            response.end(num1 + "*" + num2 + "=" + (num1*num2));
-            break;
-        case "/div":
-            response.end(num1 + "/" + num2 + "=" + (num1/num2));
-            break;
-        default:
-            response.end("Operation not supported.");
-            break;
+
+    if(isNaN(num1) || isNaN(num2)) {
+        response.end("Error.");
+    } else {
+        switch (request.url) {
+            case "/add":
+                response.end(num1 + "+" + num2 + "=" + (num1+num2));
+                break;
+            case "/sub":
+                response.end(num1 + "-" + num2 + "=" + (num1-num2));
+                break;
+            case "/mul":
+                response.end(num1 + "*" + num2 + "=" + (num1*num2));
+                break;
+            case "/div":
+                if(num2==0) {
+                    response.end("You cannot divide by 0.");
+                } else {
+                    response.end(num1 + "/" + num2 + "=" + (num1/num2));
+                }
+                break;
+            default:
+                response.end("Operation not supported.");
+                break;
+        }
     }
 }).listen(8000, '127.0.0.1');
 
